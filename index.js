58c576abb9092acf5573047d4f35230a47f479ed
Ehsan Allahi <ehsanallahi47@gmail.com>
2023-10-12 21:16:33 +0500

    Compete the code

--- index.js
+++ index.js
@@ -1,30 +1,52 @@
 import inquirer from "inquirer";
+import chalk from 'chalk';
+import figlet from 'figlet';
 async function main() {
-    let randNum = Math.floor(Math.random() * 10 + 1);
-    //   console.log("rand num is ", randNum);
+    // Generate a heading using figlet
+    console.log(chalk.blue(figlet.textSync("Number Guessing Game ", { horizontalLayout: 'full' })));
     let numTries = 3;
-    const actualNumber = randNum;
-    while (numTries > 0) {
-        const answers = await inquirer.prompt([{
-                name: "playerAnswer",
-                message: "Enter your guessed Number!",
-                type: "number"
-            }]);
-        if (answers.playerAnswer === actualNumber) {
-            console.log("Hurray! You Guessed it Right. Game ends");
-            numTries = 0;
-        }
-        else {
-            console.log("You Guessed it Wrong!");
-            if (actualNumber > answers.playerAnswer) {
-                console.log("Think Higher Number");
+    let play = true;
+    let actualNumber = Math.floor(Math.random() * 10 + 1);
+    while (play) {
+        while (numTries > 0) {
+            const answers = await inquirer.prompt([
+                {
+                    name: "playerAnswer",
+                    message: "Enter your guessed Number!",
+                    type: "number",
+                },
+            ]);
+            if (answers.playerAnswer === actualNumber) {
+                console.log(chalk.green("Hurray! You Guessed it Right. You win!🏆"));
+                numTries = 0;
             }
             else {
-                console.log("Think Lower Number");
+                console.log(chalk.red("You Guessed it Wrong!"));
+                if (actualNumber > answers.playerAnswer) {
+                    console.log(chalk.yellow("Think Higher Number"));
+                }
+                else {
+                    console.log(chalk.yellow("Think Lower Number"));
+                }
+                console.log(chalk.cyan(`You have ${numTries - 1} tries left.`));
             }
-            console.log(`You have ${numTries - 1} tries left.`);
+            numTries--;
         }
-        numTries--;
+        const playAgainAnswer = await inquirer.prompt([
+            {
+                name: "playAgain",
+                message: "Wanna play again?",
+                type: "confirm",
+            },
+        ]);
+        if (playAgainAnswer.playAgain) {
+            numTries = 3;
+            actualNumber = Math.floor(Math.random() * 10 + 1);
+        }
+        else {
+            console.log(chalk.blue(figlet.textSync("Thanks for playing!", { horizontalLayout: 'full' })));
+            play = false;
+        }
     }
 }
 main();
