bced41d111a5a32b8171da34864335860376a537
Ehsan Allahi <ehsanallahi47@gmail.com>
2023-10-12 20:07:46 +0500

    Adding Functionality of Random Number and compare with Player Guess

---
+++ index.js
@@ -0,0 +1,30 @@
+import inquirer from "inquirer";
+async function main() {
+    let randNum = Math.floor(Math.random() * 10 + 1);
+    //   console.log("rand num is ", randNum);
+    let numTries = 3;
+    const actualNumber = randNum;
+    while (numTries > 0) {
+        const answers = await inquirer.prompt([{
+                name: "playerAnswer",
+                message: "Enter your guessed Number!",
+                type: "number"
+            }]);
+        if (answers.playerAnswer === actualNumber) {
+            console.log("Hurray! You Guessed it Right. Game ends");
+            numTries = 0;
+        }
+        else {
+            console.log("You Guessed it Wrong!");
+            if (actualNumber > answers.playerAnswer) {
+                console.log("Think Higher Number");
+            }
+            else {
+                console.log("Think Lower Number");
+            }
+            console.log(`You have ${numTries - 1} tries left.`);
+        }
+        numTries--;
+    }
+}
+main();
